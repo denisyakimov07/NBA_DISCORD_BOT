3ab8f512cdf69373783ebf686a3c8e3023414616
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2021-10-28 16:03:14 -0400

    update qz

--- discord_functions.py
+++ discord_functions.py
@@ -2,8 +2,10 @@ import discord
 
 from data_base import NBA_Player, Quiz_Question, Session, session, NBA_Player
 from data_base_function import get_not_close_quiz_from_db, get_players_draft_list_from_db, add_quiz_to_db, \
-    add_guess_to_db, get_guess_to_db
-from discord_embeds import q1, wrong_guess
+    add_guess_to_db, get_guess_to_db, get_player_name_and_list_from_db, get_user_list_data
+from discord_embeds import q1, wrong_guess, player_data_card
+from fuz import spelling_check
+from helper import PlayerCard
 
 
 async def create_new_quiz(ctx: discord.ext.commands.Context):
@@ -47,3 +49,12 @@ async def checking_the_answer(ctx: discord.ext.commands.Context):
             session.commit()
             await mes.unpin()
             await create_new_quiz(ctx)
+
+async def get_user_data_by_name(ctx: discord.ext.commands.Context):
+    player_name = str(ctx.message.content).replace("!name", "").strip()
+    name_list = [pl_name.player_name for pl_name in get_player_name_and_list_from_db()]
+    matches_names_list = spelling_check(names_list=name_list, name=player_name)
+    for i in get_user_list_data(matches_names_list):
+        player = PlayerCard(i)
+        await ctx.send(embed=player_data_card(player=player, ctx=ctx))
+        await ctx.message.delete()
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from data_base import Quiz_Question, Session, session, NBA_Player
 from data_base_function import get_user_list_data, get_player_name_and_list_from_db, get_players_draft_list_from_db, \
     get_not_close_quiz_from_db, add_quiz_to_db, add_guess_to_db, get_guess_to_db
 from discord_embeds import player_data_card, q1, wrong_guess
-from discord_functions import create_new_quiz, checking_the_answer
+from discord_functions import create_new_quiz, checking_the_answer, get_user_data_by_name
 from environment import get_env
 from fuz import spelling_check
 from helper import PlayerCard
@@ -17,13 +17,7 @@ client = commands.Bot(command_prefix='!')
 
 @client.command(brief='Return NBA Player data.', description='Return NBA Player data.')
 async def name(ctx: discord.ext.commands.Context):
-    player_name = str(ctx.message.content).replace("!name", "").strip()
-    name_list = [pl_name.player_name for pl_name in get_player_name_and_list_from_db()]
-    matches_names_list = spelling_check(names_list=name_list, name=player_name)
-    for i in get_user_list_data(matches_names_list):
-        player = PlayerCard(i)
-        await ctx.send(embed=player_data_card(player=player, ctx=ctx))
-        await ctx.message.delete()
+    await get_user_data_by_name(ctx)
 
 
 
